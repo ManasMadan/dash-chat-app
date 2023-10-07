e34227c2dcc6b52b5f10c23e1a509193bc06b1a3
Manas Madan <manasmadan08@gmail.com>
2023-10-07 22:17:56 +0530

    Navbar and Color Theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 "use client";
 import "@/styles/globals.css";
 import { NextUIProvider } from "@nextui-org/react";
+import { Poppins } from "next/font/google";
+import Navbar from "@/components/Navbar";
+
+const poppins = Poppins({ weight: "500", subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -9,9 +13,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <NextUIProvider>
-        <body>{children}</body>
-      </NextUIProvider>
+      <body className={poppins.className}>
+        <NextUIProvider>
+          <Navbar />
+          {children}
+        </NextUIProvider>
+      </body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,13 +3,24 @@ const { nextui } = require("@nextui-org/react");
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {},
-  darkMode: "class",
-  plugins: [nextui()],
+  plugins: [
+    nextui({
+      themes: {
+        light: {
+          colors: {
+            background: "#151934",
+            foreground: "#ffffff",
+            focus: "#9ca3af",
+            divider: "#9ca3af",
+          },
+        },
+      },
+    }),
+  ],
 };
 export default config;
