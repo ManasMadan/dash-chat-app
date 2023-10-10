2a409ecec390a1e2fdd4ce68faf0ca1197a01d9f
Manas Madan <manasmadan08@gmail.com>
2023-10-10 16:07:12 +0530

    Removed Unused dependencies

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,21 +1,5 @@
-"use client";
-import { Button } from "@nextui-org/react";
-import { signOut } from "next-auth/react";
 import React from "react";
 
-export default function App() {
-  return (
-    <div>
-      App
-      <Button
-        color="primary"
-        variant="solid"
-        onPress={() =>
-          signOut({ callbackUrl: process.env.NEXT_PUBLIC_BASE_URL })
-        }
-      >
-        Logout
-      </Button>
-    </div>
-  );
+export default async function App() {
+  return <div>App</div>;
 }
